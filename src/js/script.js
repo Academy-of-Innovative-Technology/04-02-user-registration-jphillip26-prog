ac141fc8bccbc9853f235fa46ab3b23b346ac912
Jayden Phillip <jphillip26@aoiths.org>
2026-03-02 14:08:50 -0500

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
-document.querySelector(".btn-primary").addEventListener("click", function (event) {
+document.querySelector(".btn-primary").addEventListener("click",function (event))
 
-    const firstName = document.querySelector("");
+ var firstName = document.querySelector(".first").value;
+  var last = document.querySelector(".last").value;
+   var useremail = document.querySelector(".email").value;
+   var acc = document.querySelector(".form-check-input:checked");
+   var user = {
+    firstName: first,
+    lastName: last, 
+    useremail: email,
+    accType: acc
+   };
+   console.log(user);
 
-});
+   var u = localStorage.setItem("user", JSON.parse(user));
